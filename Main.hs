b502a1ebff632bf5624f1d7c628da052e5874343
David Waern <david.waern@gmail.com>
2016-01-26 13:50:27 +0100

    Package code take from nix script

---
+++ Main.hs
@@ -0,0 +1,96 @@
+-- A simple daemon that tails journald events, does some simple processing (see
+-- the fieldMap function), formats the events as JSON and then sends them to
+-- fluentd
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
+import Data.List ( find )
+import Data.Ratio ( (%) )
+import Data.Text.Encoding
+import Data.Time.Clock.POSIX
+import Data.Time.ISO8601
+import GHC.Generics
+import Network.Socket
+import Pipes
+import Pipes.Aeson
+import Pipes.Network.TCP ( toSocket )
+import Pipes.Safe
+import System.Environment
+import Systemd.Journal
+
+import qualified Data.Aeson as A
+import qualified Data.ByteString as BS
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as T
+import qualified Pipes.Prelude as P
+import qualified System.Posix.Files
+
+data Config = Config
+  { cursorFile :: FilePath
+  , fluentdHost :: String
+  , fluentdPort :: Int
+  } deriving (Generic, A.FromJSON)
+
+loadConfig :: IO Config
+loadConfig = do
+  configPath <- getEnv "JOURNAL2FLUENTD_CONFIG"
+  json <- BS.readFile configPath
+  case A.eitherDecodeStrict json of
+    Left err -> error err
+    Right config -> pure config
+
+main :: IO ()
+main = do
+  cfg@Config {cursorFile} <- loadConfig
+  o <- out cfg
+  hasCursor <- System.Posix.Files.fileExist cursorFile
+  start <- if hasCursor
+           then fmap (flip FromCursor Forwards) (BS.readFile cursorFile)
+           else return (FromStart) -- Forwards)
+  runSafeT $ runEffect $ src start >-> P.chain (checkpoint cursorFile) >-> encode >-> o
+
+out :: Config -> IO (Consumer BS.ByteString (SafeT IO) ())
+out (Config {fluentdHost, fluentdPort}) = do
+  addrInfo <- getAddrInfo Nothing (Just fluentdHost) (Just (show fluentdPort))
+  let serverAddr = head addrInfo
+  sock <- socket (addrFamily serverAddr) Stream defaultProtocol
+  connect sock (addrAddress serverAddr)
+  return $ toSocket sock
+
+src :: Start -> Producer JournalEntry (SafeT IO) ()
+src start = openJournal [] start Nothing Nothing
+
+encode :: Pipe JournalEntry BS.ByteString (SafeT IO) ()
+encode = for (P.map tojson) $ \e ->
+           for (encodeObject e) $ \bs -> yield bs >> yield "\n"
+
+tojson :: JournalEntry -> HM.HashMap T.Text A.Value
+tojson je = HM.fromList $ timestamp : cursor : map fieldMap kvs
+  where
+    safeUtf8 (decodeUtf8' -> Right t) = t
+    safeUtf8 _ = "[DATA]"
+    kvs = [(journalField k, safeUtf8 v) | (k,v) <- HM.toList (journalEntryFields je)]
+    cursor = ("cursor", A.String $ safeUtf8 (journalEntryCursor je))
+    timestamp = ("@timestamp"
+                , A.String $ T.pack $ formatISO8601 $
+                  posixSecondsToUTCTime $ fromRational $ us % 1000000)
+    us = case find ((== "_SOURCE_REALTIME_TIMESTAMP") . fst) kvs of
+           Just (_,v) -> read (T.unpack v)
+           Nothing -> toInteger $ journalEntryRealtime je
+
+fieldMap :: (T.Text, T.Text) -> (T.Text, A.Value)
+fieldMap ("MESSAGE", v) = ("message", A.String v)
+fieldMap ("_HOSTNAME", v) = ("host", A.String $ T.takeWhile ((/=) '.') v)
+fieldMap ("SYSLOG_IDENTIFIER", v) = ("ident", A.String v)
+fieldMap (k, v) = (T.toLower . T.dropWhile ((==) '_') $ k, A.String v)
+
+checkpoint :: String -> JournalEntry -> (SafeT IO) ()
+checkpoint cursorFile entry = liftIO $ do
+  let tmpCursorFile = cursorFile ++ ".tmp"
+  BS.writeFile tmpCursorFile (journalEntryCursor entry)
+  System.Posix.Files.rename tmpCursorFile cursorFile
---
+++ default.nix
@@ -0,0 +1,18 @@
+{ mkDerivation, aeson, base, bytestring, iso8601-time
+, libsystemd-journal, network, pipes, pipes-aeson, pipes-bytestring
+, pipes-network, pipes-safe, stdenv, text, time, unix
+, unordered-containers
+}:
+mkDerivation {
+  pname = "journal2fluentd";
+  version = "0.1.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson base bytestring iso8601-time libsystemd-journal network pipes
+    pipes-aeson pipes-bytestring pipes-network pipes-safe text time
+    unix unordered-containers
+  ];
+  license = stdenv.lib.licenses.unfree;
+}
